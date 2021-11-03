f6fa20d0a928e7140df99108a3915f3702b8f97e
Blair Robbins <blairrobbins27@gmail.com>
2021-11-02 18:20:13 -0600

    second commit of event app

--- src/components/eventForm/Form.js
+++ src/components/eventForm/Form.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react'
 
-function Form() {
+function Form(props) {
     // init values for inputs to handle updated form
     const initValues = {
-
+        title: props.title || '',
+        description: props.description || '',
+        company: props.company || '',
+        color: props.color || 'black'
     }
 
     // state handler for user inputs
-    const [eventInfo, setEventInfo] = useState({})
+    const [eventInfo, setEventInfo] = useState(initValues)
+    // destructured state for convenience
+    const {title, description, company, color} = eventInfo
+
+    // handle change for input boxes
+    function handleChange(e) {
+        const {name, value} = e.target
+        setEventInfo(prevInfo => {
+            return {
+                ...prevInfo,
+                [name]: value
+            }
+        })
+    }
 
     return (
         <div>
@@ -15,23 +31,36 @@ function Form() {
                 <input 
                     type= 'text'
                     name= 'title'
+                    value= {title}
+                    placeholder='Event Title'
+                    onChange={handleChange}
                     required
                 />
                 <input 
                     type= 'text'
                     name= 'description'
+                    value= {description}
+                    placeholder='Event Description'
+                    onChange={handleChange}
                     required
                 />
                 <input 
                     type= 'text'
                     name= 'company'
+                    value= {company}
+                    placeholder='Company Name'
+                    onChange={handleChange}
                     required
                 />
+                <span>Select Color: </span>
                 <input 
-                    type= 'text'
+                    type= 'color'
                     name= 'color'
+                    value= {color}
+                    onChange={handleChange}
                     required
                 />
+                <button>Add Event</button>
             </form>
         </div>
     )
